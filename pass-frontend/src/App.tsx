8abd69e60b8d9849943437749cb780f40da5a629
DiegoToscano04 <diego2201780@correo.uis.edu.co>
2026-02-14 02:56:37 -0500

    DATZ-Feat: App

--- pass-frontend/src/App.tsx
+++ pass-frontend/src/App.tsx
@@ -1,28 +1,15 @@
-import { Button } from "@/components/ui/button"
+import { RouterProvider } from "react-router-dom";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { router } from "@/router";
+
+const queryClient = new QueryClient();
 
 function App() {
   return (
-    <div className="flex h-screen flex-col items-center justify-center bg-slate-50 gap-4">
-      <div className="text-center space-y-2">
-        <h1 className="text-4xl font-bold text-slate-900 tracking-tight">
-          PaaS Core Education 🚀
-        </h1>
-        <p className="text-slate-500">
-          Entorno configurado con React + Vite + Shadcn/ui
-        </p>
-      </div>
-      
-      {/* 
-        AQUÍ ESTÁ EL CAMBIO: Usamos el componente <Button> 
-        en lugar de un <button> normal. 
-        Nota que las clases ya están aplicadas por Shadcn.
-      */}
-      <Button>
-        Botón de Prueba
-      </Button>
-
-    </div>
-  )
+    <QueryClientProvider client={queryClient}>
+      <RouterProvider router={router} />
+    </QueryClientProvider>
+  );
 }
 
-export default App
+export default App;
